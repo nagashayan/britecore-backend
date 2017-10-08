0daaad4cb099914b5b34e51d0fbdeea8e10417ad
Nagashayan <nagashayan1@gmail.com>
2017-10-08 19:50:12 -0400

    disabled cors for allowing cross origin policy

--- hello.py
+++ hello.py
@@ -1,9 +1,11 @@
 import os
-
+from flask_cors import CORS, cross_origin
 from app import create_app
+
 # reading this from config file
 config_name = "development"
 app = create_app(config_name)
+CORS(app)
 
 if __name__ == '__main__':
     app.run()
